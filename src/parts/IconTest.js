ee06edc8e5f07de332e4726c7393bdc5a98245fa
zalior <prabutechsolution@gmail.com>
2020-07-22 20:45:46 +0700

    make header component and put to Landing Page

---
+++ src/parts/IconTest.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Button from 'elements/button'
+
+export default function IconTest() {
+    return (
+        <Button className="brand-text-icon" href="" type="link">
+            Fun<span className="text-gray-900">stay</span>
+        </Button>
+    )
+}
