92012ebc7f1c3ee08decb2a16e4fe390a8a5d26e
zalior <prabutechsolution@gmail.com>
2020-07-25 14:33:07 +0700

    Categories View

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -3,17 +3,21 @@ import Header from 'parts/Header'
 import landingPage from 'json/landingPage.json'
 import Hero from 'parts/Hero'
 import MostPicked from 'parts/MostPicked'
+import moduleName from 'parts/Categories'
+import Categories from 'parts/Categories'
 export default class LandingPage extends Component{
     constructor(props){
         super(props);
         this.refMostPicked = React.createRef();
     }
  render(){
+     console.log(this.props);
      return (
          <>
             <Header {...this.props}></Header>
             <Hero refMostPicked={this.refMostPicked} data = {landingPage.hero} />
             <MostPicked refMostPicked={this.refMostPicked} data = {landingPage.mostPicked} />
+            <Categories data={landingPage.categories}></Categories>
          </>
      )
  }
