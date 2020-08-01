a4bc0482a96582ee2335371a92e22b9aa5529917
zalior <prabutechsolution@gmail.com>
2020-08-01 20:55:38 +0700

    Page Details Finishing

---
+++ src/elements/forms/index.js
@@ -0,0 +1,4 @@
+export { default as InputNumber } from "./inputNumber";
+export { default as InputDate } from "./inputDate";
+// export { default as InputFile } from "./InputFile";
+// export { default as InputText } from "./InputText";
